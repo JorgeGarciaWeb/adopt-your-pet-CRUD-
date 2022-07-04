df775d4edd7cefe38c17546ac11f6247f7e9162f
CarolinaArlone <carolinaarlone@gmail.com>
2022-07-04 12:21:55 +0200

    begin route pet

--- adopted/routes/index.routes.js
+++ adopted/routes/index.routes.js
@@ -11,6 +11,9 @@ router.use("/", require("./auth.routes"))
 // USER ROUTES
 // router.use("/", require("./user.routes"))
 
+//DOGS ROUTES
+router.use("/perros", require("./pet.routes"))
+
 module.exports = router;
 
 
---
+++ adopted/routes/pet.routes.js
@@ -0,0 +1,13 @@
+const router = require("express").Router()
+const Pet = require('../models/Pet.models')
+const Pound = require('../models/Pound.models')
+
+router.get('/lista', (req, res, next) => {
+    res.send('Funcionaaaaa')
+})
+
+
+
+
+
+module.exports = router
