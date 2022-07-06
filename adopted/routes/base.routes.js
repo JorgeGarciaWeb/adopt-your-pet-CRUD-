f5d8b1ee86452a4745037883796c77942e057394
Jorge Garcia <jorge.garcia.camp@outlook,com>
2022-07-06 12:06:04 +0200

    send mail

--- adopted/routes/base.routes.js
+++ adopted/routes/base.routes.js
@@ -6,40 +6,38 @@ const transporter = require('../config/transporter.config')
 router.get("/", (req, res) => res.render("index"))
 
 //CONTACT ADOPT DOG
-router.get('/contacto/:id', (req, res) => {
+router.get('/contacto/:id', (req, res, next) => {
 
     const { id } = req.params
-    console.log(id)
+
+    console.log('---------------', id)
+
     Pet
         .findById(id)
         .populate('owner')
-        .then(editPound => res.render('user/profile', editPound))
+        .then(editPound => {
+            console.log('---------->>', editPound)
+            res.render('page/contact', editPound)
+        })
         .catch(error => next(new Error(error)))
 
 })
 router.post('/contacto/:id', (req, res, next) => {
 
-
-    const { subject, message, email, name } = req.body
+    const { subject, message, email, ownerEmail } = req.body
     const { id } = req.params
+
+    console.log('---es el correo del owner---', req.session.currentUser)
 
     transporter
         .sendMail({
-<<<<<<< HEAD
-=======
-
->>>>>>> 9b52ebd4e8dea486c4da99b73b2685e427b38dbe
-            from: "adoptedyourpet@outlook.es",
-            to: email,
+            from: "<adoptedyourpet@outlook.es>",
+            to: ownerEmail,
             subject: subject,
             text: message,
 
         })
-<<<<<<< HEAD
-        .then(details => res.redirect('/'))
-=======
-        .then(details => res.redirect('perros/lista'))
->>>>>>> 9b52ebd4e8dea486c4da99b73b2685e427b38dbe
+        .then(() => res.redirect('perros/lista'))
         .catch(error => next(new Error(error)))
 
 })
